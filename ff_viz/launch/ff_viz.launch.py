f4f01470f01907e9e23dcddcad4cb654c7a7d0b3
Alvin Sun <alvinsunyixiao@gmail.com>
2023-02-13 12:12:13 -0800

    add in msg and use xacro

--- ff_viz/launch/ff_viz.launch.py
+++ ff_viz/launch/ff_viz.launch.py
@@ -2,25 +2,34 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterValue
 
 def generate_launch_description():
-    urdf_file_name = "ff.urdf"
-    urdf = os.path.join(get_package_share_directory("ff_viz"), "model", urdf_file_name)
-    with open(urdf, "r") as f:
-        robot_desc = f.read()
+    urdf = os.path.join(get_package_share_directory("ff_viz"), "model", "ff.urdf.xacro")
+    robot_name = LaunchConfiguration("robot_name")
 
     return LaunchDescription([
+        DeclareLaunchArgument("robot_name", default_value="robot"),
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
             name="robot_state_publisher",
-            parameters=[{"robot_description": robot_desc}],
+            namespace=robot_name,
+            parameters=[{
+                "robot_description": ParameterValue(
+                    Command(["xacro ", urdf, " robot_name:=", robot_name]),
+                    value_type=str
+                )
+            }],
         ),
         Node(
             package="ff_viz",
             executable="renderer_node",
             name="renderer_node",
+            parameters=[{"robot_names": [robot_name]}]
         ),
         Node(
             package="rviz2",
--- ff_viz/src/renderer_node.cpp
+++ ff_viz/src/renderer_node.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <functional>
 #include <memory>
+#include <string>
+#include <vector>
 
 #include <rclcpp/rclcpp.hpp>
 
@@ -11,11 +13,14 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include <visualization_msgs/msg/marker.hpp>
 
+#include "ff_msgs/msg/pose2_d_stamped.hpp"
+
 using namespace std::chrono_literals;
 
 class TableRenderer : public rclcpp::Node {
  public:
   TableRenderer(const std::string& name) : rclcpp::Node(name) {
+    auto robot_names = this->declare_parameter<std::vector<std::string>>("robot_name", {"robot"});
     // build marker for the table
     marker_msg_.ns = "table";
     marker_msg_.id = 0;
@@ -24,7 +29,7 @@ class TableRenderer : public rclcpp::Node {
     marker_msg_.scale.x = 0.001;
     marker_msg_.scale.y = 0.001;
     marker_msg_.scale.z = 0.001;
-    marker_msg_.header.frame_id = "/map";
+    marker_msg_.header.frame_id = "map";
     marker_msg_.action = visualization_msgs::msg::Marker::ADD;
     marker_msg_.pose.position.x = 0;
     marker_msg_.pose.position.y = 2.7;
@@ -41,7 +46,7 @@ class TableRenderer : public rclcpp::Node {
 
     // build pose msg
     transform_msg_.header.frame_id = "map";
-    transform_msg_.child_frame_id = "robot/base";
+    transform_msg_.child_frame_id = robot_names[0] + "/base";
     transform_msg_.transform.rotation.x = 0.0;
     transform_msg_.transform.rotation.y = 0.0;
     transform_msg_.transform.rotation.z = 0.0;
@@ -49,6 +54,12 @@ class TableRenderer : public rclcpp::Node {
     transform_msg_.transform.translation.x = 0.0;
     transform_msg_.transform.translation.y = 0.0;
     transform_msg_.transform.translation.z = 0.0;
+
+    pose_sub_ = this->create_subscription<ff_msgs::msg::Pose2DStamped>(
+      "/robot/estimator/pose",
+      10,
+      std::bind(&TableRenderer::PoseCallback, this, std::placeholders::_1)
+    );
 
     // marker publisher
     marker_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("table_marker", 10);
@@ -62,6 +73,7 @@ class TableRenderer : public rclcpp::Node {
 
  private:
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Subscription<ff_msgs::msg::Pose2DStamped>::SharedPtr pose_sub_;
 
   visualization_msgs::msg::Marker marker_msg_;
   rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
@@ -76,6 +88,19 @@ class TableRenderer : public rclcpp::Node {
 
     // render robot state
     tf_broadcaster_->sendTransform(transform_msg_);
+  }
+
+  void PoseCallback(const ff_msgs::msg::Pose2DStamped::SharedPtr msg) {
+    transform_msg_.transform.translation.x = msg->x;
+    transform_msg_.transform.translation.y = msg->y;
+    transform_msg_.transform.translation.z = 0.15;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, msg->theta);
+    transform_msg_.transform.rotation.x = q.x();
+    transform_msg_.transform.rotation.y = q.y();
+    transform_msg_.transform.rotation.z = q.z();
+    transform_msg_.transform.rotation.w = q.w();
   }
 };
 
