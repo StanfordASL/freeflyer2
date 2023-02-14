a3870fdf7d96c0868f76c205169974c05fb19b98
Alvin Sun <alvinsunyixiao@gmail.com>
2023-02-14 15:10:37 -0800

    add c++ param utils

---
+++ ff_params/include/ff_params/robot_params.hpp
@@ -0,0 +1,50 @@
+#ifndef ROBOT_PARAMS_HPP_
+#define ROBOT_PARAMS_HPP_
+
+#include <atomic>
+#include <future>
+#include <string>
+#include <vector>
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace ff {
+
+struct DynamicsParams {
+  double mass;
+  double inertia;
+  double radius;
+  std::vector<double> CoM_offset;
+  std::vector<double> force_const;
+};
+
+struct ActuatorParams {
+  double F_max_per_thruster;
+  double thrusters_lever_arm;
+  double F_body_max;
+  double M_body_max;
+  double min_inp_percent;
+  double max_inp_percent;
+  double gamma_min;
+  double gamma_max;
+};
+
+class RobotParams {
+ public:
+  DynamicsParams dynamics;
+  ActuatorParams actuators;
+
+  RobotParams(rclcpp::Node* node, const std::string& robot_name);
+
+  bool Loaded() const;
+
+ private:
+  std::atomic<bool> loaded_ = false;
+  rclcpp::AsyncParametersClient::SharedPtr param_client_;
+
+  void ParamReadyCallback(std::shared_future<std::vector<rclcpp::Parameter>> future);
+};
+
+} // namespace ff
+
+#endif // ROBOT_PARAMS_HPP_
---
+++ ff_params/src/robot_params.cpp
@@ -0,0 +1,61 @@
+#include "ff_params/robot_params.hpp"
+
+#include <chrono>
+
+using namespace std::chrono_literals;
+using namespace std::placeholders;
+
+namespace ff {
+
+RobotParams::RobotParams(rclcpp::Node* node, const std::string& robot_name)
+  : loaded_(false),
+    param_client_(std::make_shared<rclcpp::AsyncParametersClient>(
+      node, "/" + robot_name + "/robot_params_node")) {
+  while (!param_client_->service_is_ready()) {
+    param_client_->wait_for_service(5s);
+    RCLCPP_INFO(node->get_logger(), "parameter service not ready, retrying...");
+  }
+
+  param_client_->get_parameters({
+    "dynamics.mass",
+    "dynamics.inertia",
+    "dynamics.radius",
+    "dynamics.CoM_offset",
+    "dynamics.force_const",
+    "actuators.F_max_per_thruster",
+    "actuators.thrusters_lever_arm",
+    "actuators.F_body_max",
+    "actuators.M_body_max",
+    "actuators.min_inp_percent",
+    "actuators.max_inp_percent",
+    "actuators.gamma_min",
+    "actuators.gamma_max",
+  }, std::bind(&RobotParams::ParamReadyCallback, this, _1));
+}
+
+bool RobotParams::Loaded() const {
+  return loaded_.load();
+}
+
+void RobotParams::ParamReadyCallback(std::shared_future<std::vector<rclcpp::Parameter>> future) {
+  const auto params = future.get();
+
+  this->dynamics.mass = params[0].as_double();
+  this->dynamics.inertia = params[1].as_double();
+  this->dynamics.radius = params[2].as_double();
+  this->dynamics.CoM_offset = params[3].as_double_array();
+  this->dynamics.force_const = params[4].as_double_array();
+
+  this->actuators.F_max_per_thruster = params[5].as_double();
+  this->actuators.thrusters_lever_arm = params[6].as_double();
+  this->actuators.F_body_max = params[7].as_double();
+  this->actuators.M_body_max = params[9].as_double();
+  this->actuators.min_inp_percent = params[9].as_double();
+  this->actuators.max_inp_percent = params[10].as_double();
+  this->actuators.gamma_min = params[11].as_double();
+  this->actuators.gamma_max = params[12].as_double();
+
+  loaded_.store(true);
+}
+
+} // namespace ff
