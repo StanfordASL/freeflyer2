74fc227dfb98b367bba37a32373ba9aed49e5c50
Alvin Sun <alvinsunyixiao@gmail.com>
2023-02-15 00:54:43 -0800

    add top level package

--- ff_sim/setup.py
+++ ff_sim/setup.py
@@ -11,7 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, glob('launch/*launch.[pxy][yma]*')),
+        ('share/' + package_name + "/launch", glob('launch/*launch.[pxy][yma]*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
---
+++ freeflyer/README.md
@@ -0,0 +1,10 @@
+# freeflyer package
+
+Top level package that launches multiple modules together.
+
+### Quick Examples
+
+**PD Waypoint Control in Simulator**
+```sh
+ros2 launch freeflyer sim_pd.launch.py
+```
