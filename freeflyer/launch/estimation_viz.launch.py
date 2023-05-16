b33b992a34862d56f4bfc78fa19ff82a44c44ab9
Alvin Sun <alvinsunyixiao@gmail.com>
2023-05-16 16:09:18 -0700

    pull changes from driver branch

---
+++ freeflyer/launch/estimation_viz.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    robot_name = LaunchConfiguration("robot_name")
+
+    return LaunchDescription([
+        DeclareLaunchArgument("robot_name", default_value="robot"),
+        IncludeLaunchDescription(
+            PathJoinSubstitution([
+                FindPackageShare("ff_viz"),
+                "launch",
+                "ff_viz.launch.py",
+            ]),
+            launch_arguments={"robot_name": robot_name}.items(),
+        ),
+        Node(
+            package="ff_estimate",
+            executable="mocap_estimator_node",
+            name="mocap_estimator_node",
+            namespace=robot_name,
+            parameters=[{
+                "pose_channel": PathJoinSubstitution([
+                    "mocap",
+                    robot_name,
+                    "pose",
+                ]),
+            }],
+        ),
+    ])
