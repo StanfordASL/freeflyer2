c77ba03820305c3b1dfbc8765d743598e0cd9c7d
Alvin Sun <alvinsunyixiao@gmail.com>
2023-05-17 11:23:28 -0700

    fix style for ff_control

--- ff_control/src/nodes/key_teleop_node.cpp
+++ ff_control/src/nodes/key_teleop_node.cpp
@@ -1,3 +1,26 @@
+// MIT License
+//
+// Copyright (c) 2023 Stanford Autonomous Systems Lab
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
 #include <fcntl.h>
 #include <termios.h>
 #include <chrono>
@@ -13,7 +36,8 @@
 
 using namespace std::chrono_literals;
 
-const std::string help_msg = R"(
+const char help_msg[] =
+  R"(
 This keyboard teleop node controls the linear and angular velocity of the
 freeflyer using a simple proportional controller. There will be large delay
 in your control actions due to low control authority.
@@ -31,12 +55,14 @@ A/D -- translate the robot in positive / negative y direction
 Q/E -- rotate the robot (q counter clockwise, e clockwise)
 )";
 
-class KeyboardTeleopNode : public ff::LinearController, public ff::KeyboardController {
- public:
+class KeyboardTeleopNode : public ff::LinearController, public ff::KeyboardController
+{
+public:
   KeyboardTeleopNode()
-    : rclcpp::Node("key_teleop_node"),
-      ff::LinearController(),
-      ff::KeyboardController() {
+  : rclcpp::Node("key_teleop_node"),
+    ff::LinearController(),
+    ff::KeyboardController()
+  {
     // start teleop loop
     telop_timer_ = this->create_wall_timer(10ms, std::bind(&KeyboardTeleopNode::TeleopLoop, this));
 
@@ -49,44 +75,46 @@ class KeyboardTeleopNode : public ff::LinearController, public ff::KeyboardContr
     K_(2, 5) = gain_dt;
 
     // print help message
-    RCLCPP_INFO(this->get_logger(), "%s", help_msg.c_str());
+    RCLCPP_INFO(this->get_logger(), "%s", help_msg);
   }
 
- private:
+private:
   rclcpp::TimerBase::SharedPtr telop_timer_;
 
   FeedbackMat K_;
 
-  void TeleopLoop() {
-    if (!StateIsReady()) { return; }
+  void TeleopLoop()
+  {
+    if (!StateIsReady()) {return;}
 
     ff_msgs::msg::FreeFlyerState state_des{};
     switch (this->GetKey()) {
-    case 'w':
-      state_des.twist.vx = 0.3;
-      break;
-    case 's':
-      state_des.twist.vx = -0.3;
-      break;
-    case 'a':
-      state_des.twist.vy = 0.3;
-      break;
-    case 'd':
-      state_des.twist.vy = -0.3;
-      break;
-    case 'q':
-      state_des.twist.wz = 0.5;
-      break;
-    case 'e':
-      state_des.twist.wz = -0.5;
-      break;
+      case 'w':
+        state_des.twist.vx = 0.3;
+        break;
+      case 's':
+        state_des.twist.vx = -0.3;
+        break;
+      case 'a':
+        state_des.twist.vy = 0.3;
+        break;
+      case 'd':
+        state_des.twist.vy = -0.3;
+        break;
+      case 'q':
+        state_des.twist.wz = 0.5;
+        break;
+      case 'e':
+        state_des.twist.wz = -0.5;
+        break;
     }
 
     SendControl(state_des, K_);
   }
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char ** argv)
+{
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<KeyboardTeleopNode>());
   rclcpp::shutdown();
