841f604ec03b4d056f2c7055997756fccffb2c98
Robert Dyro <rdyro@stanford.edu>
2023-07-10 14:45:34 -0700

    fixing automated test passing

--- ff_path_planning/ff_path_planning/import_costs_and_dynamics.py
+++ ff_path_planning/ff_path_planning/import_costs_and_dynamics.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2023 Stanford Autonomous Systems Lab
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import sys
 import os
 from typing import Optional, Union
@@ -56,13 +78,13 @@ def append_to_path(root_path: Optional[Union[Path, str]] = None):
             sys.path.append(str(path))
 
     # insert the local development path first #########################
-    #pkg_path = find_pkg_source_dir(root_path)
-    #this_pkg_name = root_path.parts[-1]
-    #msg = f"We're attempting to guess the package path is {pkg_path}. Is this correct?"
-    #dynamics_path = pkg_path / this_pkg_name / "dynamics"
-    #costs_path = pkg_path / this_pkg_name / "costs"
-    #assert dynamics_path.exists() and costs_path.exists(), msg
-    #for path in [dynamics_path, costs_path]:
+    # pkg_path = find_pkg_source_dir(root_path)
+    # this_pkg_name = root_path.parts[-1]
+    # msg = f"We're attempting to guess the package path is {pkg_path}. Is this correct?"
+    # dynamics_path = pkg_path / this_pkg_name / "dynamics"
+    # costs_path = pkg_path / this_pkg_name / "costs"
+    # assert dynamics_path.exists() and costs_path.exists(), msg
+    # for path in [dynamics_path, costs_path]:
     #    if str(path) not in sys.path:
     #        sys.path.insert(0, str(path))
 
@@ -70,10 +92,10 @@ def append_to_path(root_path: Optional[Union[Path, str]] = None):
 def load_all_modules(root_path: Optional[Union[Path, str]] = None):
     all_dynamics, all_costs = find_all_dynamics_and_costs(root_path)
 
-    #pkg_source = find_pkg_source_dir(root_path)
-    #all_dynamics_dev, all_costs_dev = find_all_dynamics_and_costs(pkg_source / pkg_source.parts[-1])
-    #all_dynamics += all_dynamics_dev
-    #all_costs += all_costs_dev
+    # pkg_source = find_pkg_source_dir(root_path)
+    # all_dynamics_dev, all_costs_dev = find_all_dynamics_and_costs(pkg_source / pkg_source.parts[-1])
+    # all_dynamics += all_dynamics_dev
+    # all_costs += all_costs_dev
 
     for dynamics in all_dynamics:
         mod_name = str(Path(dynamics).stem)
