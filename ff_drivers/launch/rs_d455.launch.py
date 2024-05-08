fa0302cd838d81986ed9fae4ddb1b16819e30827
Andrew Wang <aw1605@berkeley.edu>
2024-05-08 16:08:28 -0700

    Fix typo

--- ff_drivers/launch/rs_d455.launch.py
+++ ff_drivers/launch/rs_d455.launch.py
@@ -44,7 +44,7 @@ def generate_launch_description():
                         "rgb_camera.profile": "640x360x30",
                         "rgb_camera.enable_auto_exposure": False,
                         "rgb_camera.enable_auto_white_balance": False,
-                        "rgb_camera.white_balance": 3800.0
+                        "rgb_camera.white_balance": 3800.0,
                         "enable_infra1": False,
                         "enable_infra2": False,
                     }
